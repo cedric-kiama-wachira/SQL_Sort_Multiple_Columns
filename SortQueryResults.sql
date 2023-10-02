673e7f25da16c4c78095984ec2280f63e17e2619
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-10-02 23:47:31 +0400

    updated my script file with basiq query and adding sort options

--- SortQueryResults.sql
+++ SortQueryResults.sql
@@ -11,3 +11,24 @@ FROM dvd_rentals.sales_by_film_category
 ORDER BY 1
 LIMIT 5;
 
+/*Sort By Descending - What are the first 5 values in reverse alphabetical order in the country column from the country table?*/
+SELECT country
+FROM dvd_rentals.country
+ORDER BY country DESC
+LIMIT 5;
+
+/*Which category had the lowest total_sales value according to the sales_by_film_category table? What was the total_sales value?*/
+SELECT
+  category,
+  total_sales
+FROM dvd_rentals.sales_by_film_category
+ORDER BY total_sales
+LIMIT 1;
+
+/*What was the latest payment_date of all dvd rentals in the payment table?*/
+SELECT
+  payment_date
+FROM dvd_rentals.payment
+ORDER BY payment_date DESC
+LIMIT 1;
+
